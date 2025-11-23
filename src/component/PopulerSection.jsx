17784c6e9bc6935546a5f030bab16f2f829266d2
mkhshohud99 <mkhshohud1999@gmail.com>
2025-11-24 01:10:32 +0600

    first commit

---
+++ src/component/PopulerSection.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PopulerSection = () => {
+    return (
+        <div>
+            <div>
+                <h3>Popular pet Section</h3>
+            </div>
+        </div>
+    );
+};
+
+export default PopulerSection;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Slider from '../component/Slider';
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+const Home = () => {
+    return (
+        <div>
+            <Slider></Slider>
+        </div>
+    );
+};
+
+export default Home;
